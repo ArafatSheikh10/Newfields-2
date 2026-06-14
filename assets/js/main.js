53543effc11e4478cc53910a4dbac8554851422d
Arafat <placeholderause4590@gmail.com>
2026-06-14 20:25:39 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,76 @@
+(function ($) {
+    "use strict";
+
+
+    jQuery(document).ready(function ($) {
+
+
+
+
+        // AOS Init
+        AOS.init({
+            once: true,
+            duration: 1500,
+        });
+
+
+        // Nav Menu Active 
+        $('.header__menu ul li a').click(function(){
+            $('li a').removeClass("active");
+            $(this).addClass("active");
+        });
+
+        
+        // Header Menu Collapse
+        $(".menu__trigger").on("click", function () {
+            $(".menu-list").toggleClass("active");
+        });
+
+
+        $('.featured__card--wraper').owlCarousel({
+            loop:               true,
+            margin:             20,
+            items:              1,
+            autoplay:           true,
+            autoplayTimeout:    2000,
+            smartSpeed:         1200,
+            autoplayHoverPause: true,
+            responsive: {
+              0: {
+                stagePadding: 60,   
+              },
+              575: {
+                stagePadding: 100,   
+              },
+              767: {
+                stagePadding: 150,
+              },
+              992: {
+                stagePadding: 250,
+              },
+              1199: {
+                stagePadding: 350,
+              },
+              1449: {
+                stagePadding: 400,
+              },
+              1620: {
+                stagePadding: 600,
+              },
+            },
+        });
+
+
+        // Get the current year
+        const currentYear = new Date().getFullYear();
+        document.querySelector(".copyright-year").textContent = currentYear;
+
+
+
+    }); //---document-ready-----
+
+
+
+}(jQuery));
+
+
